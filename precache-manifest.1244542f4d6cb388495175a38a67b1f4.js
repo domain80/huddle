7776ef2b7b5e211ceec44826e743d6d42a3f2fed
David-Main <work.davidmainoo@gmail.com>
2021-12-27 00:02:54 +0000

    Updates

---
+++ precache-manifest.1244542f4d6cb388495175a38a67b1f4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0e96362b416fad5502a",
+    "url": "/huddle/static/js/main.d0e96362.chunk.js"
+  },
+  {
+    "revision": "b0b17ac34351a90ed05e",
+    "url": "/huddle/static/js/1.b0b17ac3.chunk.js"
+  },
+  {
+    "revision": "429adb545bb690e08811",
+    "url": "/huddle/static/js/runtime~main.429adb54.js"
+  },
+  {
+    "revision": "fc2b0e78644c549e3d046357413493fc",
+    "url": "/huddle/index.html"
+  }
+];
